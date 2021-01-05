e8f6c6fc44e9ca5b1ee8076fbc0a0a172e443c89
mkhoirulwafa <masapaapa@gmail.com>
2021-01-06 00:15:55 +0700

    ✨ add GET and POST user (need to be fixed soon)

--- routers/users.js
+++ routers/users.js
@@ -3,10 +3,24 @@ const router = express.Router();
 const { Users } = require("../models/user");
 
 router.get(`/`, async (req, res) => {
-  const productList = await Users.find();
-  if (!productList) {
+  const usersList = await Users.find();
+  if (!usersList) {
   }
-  res.send(productList);
+  res.send(usersList);
+});
+
+router.post(`/`, async (req, res) => {
+  const { name, icon, color } = req.body;
+  let user = new Users({
+    name: name,
+    icon: icon,
+    color: color,
+  });
+  user = await user.save();
+  if (!user) {
+    return res.status(500).send("Register User cannot be Success!");
+  }
+  res.status(201).json(user);
 });
 
 module.exports = router;
